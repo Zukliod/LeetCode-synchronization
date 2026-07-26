a6c358ebd82a47b9ab96b06e814ed7e2f6de1b80
harshit <156461915+Zukliod@users.noreply.github.com>
2026-07-26 05:50:13 +0530

    Refactor logging in fetch_leetcode_stats function

--- scripts/github_utils.py
+++ scripts/github_utils.py
@@ -1,10 +1,15 @@
 from config import LEETCODE_USERNAME
-from utils import get_json, logger
+
+from utils import get_json
+
+from utils import log
+
+from utils import error
 
 
 def fetch_leetcode_stats(username=LEETCODE_USERNAME):
 
-    logger.info("Fetching LeetCode Stats...")
+    log("Fetching LeetCode Stats...")
 
     url = (
         f"https://leetcode-api-faisalshohag.vercel.app/"
@@ -16,17 +21,23 @@ def fetch_leetcode_stats(username=LEETCODE_USERNAME):
         data = get_json(url)
 
         stats = {
+
             "totalSolved": data.get("totalSolved", 0),
+
             "easySolved": data.get("easySolved", 0),
+
             "mediumSolved": data.get("mediumSolved", 0),
+
             "hardSolved": data.get("hardSolved", 0),
+
             "ranking": data.get("ranking", "N/A")
+
         }
 
         return stats
 
     except Exception as e:
 
-        logger.error(f"Unable to fetch LeetCode Stats: {e}")
+        error(f"Unable to fetch LeetCode Stats: {e}")
 
         return None
